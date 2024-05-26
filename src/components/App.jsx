3dd1542f2fd67d83e0cbd071bc3bcc9baf51dc01
Ewa <efaltynska@gmail.com>
2024-05-26 10:47:07 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,61 +1,64 @@
-import { Component } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import ContactForm from './ContactForm/ContactForm';
 import Filter from './Filter/Filter';
 import ContactList from './ContactList/ContactList';
 import s from './App.module.css';
+import { useRef } from 'react';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
+export const App = () => {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('contacts')) ?? []
+  );
+  const [filter, setFilter] = useState('');
+  const firstStartRef = useRef(false);
+
+  useEffect(() => {
+    if (firstStartRef.current) {
+      const data = JSON.stringify(contacts);
+      localStorage.setItem('contacts', data);
+    }
+    firstStartRef.current = true;
+  }, [contacts]);
+
+  const handleSubmitForm = obj => {
+    setContacts(prev => [...prev, obj]);
   };
 
-  handleSubmitForm = obj => {
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, { ...obj }],
-    }));
+  const handleChangeInput = e => {
+    setFilter(e.target.value);
   };
 
-  handleChangeInput = e => {
-    const input = e.target;
-
-    this.setState({ [input.name]: input.value });
+  const handleDeleteContacts = id => {
+    setContacts(contacts.filter(el => el.id !== id));
   };
 
-  handleDeleteContacts = id => {
-    this.setState({ contacts: this.state.contacts.filter(el => el.id !== id) });
-  };
+  const filterList = useMemo(
+    () =>
+      contacts.filter(el =>
+        el.name.toLowerCase().includes(filter.toLocaleLowerCase())
+      ),
+    [filter, contacts]
+  );
 
-  render() {
-    const { contacts, filter } = this.state;
-    return (
-      <div className={s.wrap}>
-        <div className={s.phonebook}>
-          <h1 className={s.title}>Phonebook</h1>
-          <ContactForm
-            contacts={contacts}
-            handleSubmitForm={this.handleSubmitForm}
-          />
-          <Filter
-            title="Find contacts by name"
-            handleChangeInput={this.handleChangeInput}
-          />
-        </div>
-
-        <div className={s.contacts}>
-          <h2 className={s.subTitle}>Contacts: {contacts.length}</h2>
-          <ContactList
-            filter={filter}
-            contacts={contacts}
-            handleDeleteContacts={this.handleDeleteContacts}
-          />
-        </div>
+  return (
+    <div className={s.wrap}>
+      <div className={s.phonebook}>
+        <h1 className={s.title}>Phonebook</h1>
+        <ContactForm contacts={contacts} handleSubmitForm={handleSubmitForm} />
+        <Filter
+          title="Find contacts by name"
+          value={filter}
+          handleChangeInput={handleChangeInput}
+        />
       </div>
-    );
-  }
-}
+
+      <div className={s.contacts}>
+        <h2 className={s.subTitle}>Contacts: {contacts.length}</h2>
+        <ContactList
+          contacts={filterList}
+          handleDeleteContacts={handleDeleteContacts}
+        />
+      </div>
+    </div>
+  );
+};
